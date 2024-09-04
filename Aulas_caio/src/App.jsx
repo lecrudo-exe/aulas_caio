4036323167bc74947baa058418b193fa5d146c64
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-04 12:28:28 -0300

    90%

---
+++ Aulas_caio/src/App.jsx
@@ -0,0 +1,36 @@
+function App() {
+ 
+ 
+  return (
+    <>
+    <nav className='bg-pink-500 bg-cover w-full h-28 '></nav>
+      <div className=" h-screen">
+        <div className="flex flex-row">
+        <div className="bg-blue-700 w-2/12 h-screen p-10" >
+          <div className="bg-pink-500 bg-cover w-28 h-28  flex justify-center items-center mb-10"></div>
+          <div className="bg-pink-500 bg-cover w-28 h-28 flex justify-center items-center mb-10 "></div>
+          <div className="bg-pink-500 bg-cover w-28 h-28  flex justify-center items-center mb-10"></div>
+        </div>
+ 
+        <div className="w-8/12 grid grid-rows-3 grid-flow-col gap-4 justify-around items-center">
+          <div className="bg-pink-500 w-28 h-28"></div>
+          <div className="bg-pink-500 w-28 h-28"></div>
+          <div className="bg-pink-500 w-28 h-28"></div>
+          <div className="bg-pink-500 w-28 h-28"></div>
+          <div className="bg-pink-500 w-28 h-28"></div>
+          <div className="bg-pink-500 w-28 h-28"></div>
+          <div className="bg-pink-500 w-28 h-28"></div>
+          <div className="bg-pink-500 w-28 h-28"></div>
+          <div className="bg-pink-500 w-28 h-28"></div>
+ 
+        </div>
+        </div>
+       
+       
+      </div>
+ 
+    </>
+  )
+}
+ 
+export default App
