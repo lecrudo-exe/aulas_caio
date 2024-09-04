ac336c16e2ee5992e43e1d6f874e46fb9cac17cc
Laboratório FIAP <labsfiap@fiap.com.br>
2024-09-04 12:34:30 -0300

    terminado

--- Aulas_caio/src/App.jsx
+++ Aulas_caio/src/App.jsx
@@ -6,10 +6,10 @@ function App() {
     <nav className='bg-pink-500 bg-cover w-full h-28 '></nav>
       <div className=" h-screen">
         <div className="flex flex-row">
-        <div className="bg-blue-700 w-2/12 h-screen p-10" >
-          <div className="bg-pink-500 bg-cover w-28 h-28  flex justify-center items-center mb-10"></div>
-          <div className="bg-pink-500 bg-cover w-28 h-28 flex justify-center items-center mb-10 "></div>
-          <div className="bg-pink-500 bg-cover w-28 h-28  flex justify-center items-center mb-10"></div>
+        <div className="bg-blue-700 w-48 h-screen grid grid-cols-1 gap-4 items-center px-8" >
+          <div className="bg-pink-500 bg-cover w-28 h-28  flex justify-center items-center"></div>
+          <div className="bg-pink-500 bg-cover w-28 h-28 flex justify-center items-center"></div>
+          <div className="bg-pink-500 bg-cover w-28 h-28  flex justify-center items-center"></div>
         </div>
  
         <div className="w-8/12 grid grid-rows-3 grid-flow-col gap-4 justify-around items-center">
